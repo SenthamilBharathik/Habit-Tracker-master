b9ec50c93eb6d9f04295959d89337b73375d16df
Senthamil Bharathi K <senthamilbharathi12@gmail.com>
2024-12-05 20:46:43 +0530

    Habit Tracker

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+    'cypress/globals': true,
+  },
+  extends: [
+  ],
+  parserOptions: {
+  },
+  plugins: ['react', 'jest', 'cypress'],
+  rules: {
+  },
+};
